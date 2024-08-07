6419af29306973336c8fa525bd0ed67d77c5cb43
Maximiliano Mie <130712564+miemaximiliano@users.noreply.github.com>
2024-08-07 00:00:00 -0300

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,16 @@ import reservasRouter from "./routes/reservas";
 
 const app: Express = express();
 
+// Configuración CORS
+const corsOptions = {
+  origin: 'https://track-day-s-reservas-front.vercel.app', 
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use(cors());
 
 // Rutas de la app
 app.use('/eventos', eventosRouter);
@@ -15,11 +23,11 @@ app.use('/usuarios', usuariosRouter);
 app.use('/reservas', reservasRouter);
 
 app.get('/', (req, res: Response) => {
-	res.status(200).send({
-		message: 'Server is up ✅ - Environment: develop',
-		data: undefined,
-		error: false,
-	});
+  res.status(200).send({
+    message: 'Server is up ✅ - Environment: develop',
+    data: undefined,
+    error: false,
+  });
 });
 
 export default app;
